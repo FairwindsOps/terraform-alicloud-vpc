1634ff5cae64ff425995f52c033c9af00b86bfad
John Wynkoop <jlwynkoop@gmail.com>
2017-11-20 12:39:10 -0500

    adding 80/443 to sg

--- sg.tf
+++ sg.tf
@@ -29,6 +29,28 @@ resource "alicloud_security_group_rule" "ssh-in" {
   cidr_ip = "0.0.0.0/0"
 }
 
+resource "alicloud_security_group_rule" "http-in" {
+  type = "ingress"
+  ip_protocol = "tcp"
+  nic_type = "intranet"
+  policy = "accept"
+  port_range = "80/80"
+  priority = 2
+  security_group_id = "${alicloud_security_group.vpc-sg.id}"
+  cidr_ip = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "https-in" {
+  type = "ingress"
+  ip_protocol = "tcp"
+  nic_type = "intranet"
+  policy = "accept"
+  port_range = "443/443"
+  priority = 3
+  security_group_id = "${alicloud_security_group.vpc-sg.id}"
+  cidr_ip = "0.0.0.0/0"
+}
+
 output "sg_id" {
   value = "${alicloud_security_group.vpc-sg.id}"
 }
